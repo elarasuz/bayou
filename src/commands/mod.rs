865b24203fe3f0de8749da2b703a927053703544
Eyal Arasu <elarasuz@gmail.com>
2023-08-20 00:30:26 -0700

    mqtt subscribe to a topic and write to file

---
+++ src/commands/mod.rs
@@ -0,0 +1,18 @@
+extern crate log;
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+pub struct Cli {
+    /// config file
+    #[arg(short, long, value_name="config file", default_value="")]
+    pub config: String,
+
+    // #[command(subcommand)]
+    // pub command: Commands,
+}
+
+// #[derive(Subcommand)]
+// pub enum Commands {
+// }
